93c336bef4d95ab3feba7d0df9f4578dca77c5e4
LouisRauch <69316801+LouisRauch@users.noreply.github.com>
2021-05-11 08:52:29 -0500

    finished project

--- project/radix-sort.py
+++ project/radix-sort.py
@@ -1,4 +1,5 @@
 import urllib
+import random
 import urllib.request
 
 def book_to_words(book_url='https://www.gutenberg.org/files/84/84-0.txt'):
@@ -7,11 +8,45 @@ def book_to_words(book_url='https://www.gutenberg.org/files/84/84-0.txt'):
     return bookascii.split()
 
 def radix_a_book(book_url='https://www.gutenberg.org/files/84/84-0.txt'):
-    l = book_to_words()
+    l = book_to_words(book_url)
     maximum = max(l)
+    l = padLength(l,maximum)
     for i in range(maximum):
         l = toArray(toQueue(l,i),len(l))
+    l = changeAlphabetical(l)
+    l = unPadList(l)
     return l
+
+def padLength(lst,length):
+    arr = []
+    fix = " "
+    fixed = fix.encode('ascii','replace')
+    for i in range(len(lst)):
+        v = lst[i]
+        while not len(v) == length:
+            v = v + fixed
+        arr.append(v)
+    return arr
+
+def unPadList(lst):
+    arr = []
+    for i in range(len(lst)):
+        v = lst[i]
+        v = v.strip(b' ')
+        arr.append(v)
+    return arr
+
+
+def changeAlphabetical(lst):
+    queue = []
+    for i in range(256):
+        l = []
+        queue.append(l)
+    for j in range(len(lst)):
+        n = lst[j][0]
+        queue[n].append(lst[j])
+    sort = toArray(queue,len(lst))
+    return sort
 
 def max(lst):
     maximum = len(lst[0])
@@ -57,6 +92,25 @@ def getPlace(val,k):
         f = k - 1
         return getPlace(val[0:len(val)-1],f)
 
+################################################################################
+# TEST CASES
+################################################################################
+
+def test_book():
+    arr = book_to_words()
+    lst = radix_a_book()
+    arr = sorted(arr)
+    n = 0
+    for i in range(len(arr)):
+        if not arr[i]==lst[i]:
+            print("Python Sort")
+            print(arr[i])
+            print()
+            print("Radix Sort")
+            print(lst[i])
+            raise Exception
+        n+=1
+        print("Correct Comparisons = " + str(n))
 
 
 
@@ -70,10 +124,10 @@ def say_success():
 # MAIN
 ################################################################################
 def main():
-    l = radix_a_book()
-    for i in range(len(l)):
-        if i%100==0:
-            print(l[i])
+    for t in [test_book]:
+        say_test(t)
+        t()
+        say_success()
     print(80 * "#" + "\nALL TEST CASES FINISHED SUCCESSFULLY!\n" + 80 * "#")
 
 if __name__ == '__main__':
